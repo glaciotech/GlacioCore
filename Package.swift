ac198cfff57ae4e872a3568610db231f5b8e4beb
Peter Liddle <development.peter@liddle.network>
2023-04-12 14:23:07 -0500

    Updated Package.swift for pre-release v0.11.0

--- Package.swift
+++ Package.swift
@@ -14,17 +14,16 @@ let package = Package(
     targets: [
         .target(name: "GlacioCoreWrapper", dependencies: ["GlacioCore", "MessagePack", "ArgumentParser", "ArgumentParserToolInfo"], path: "Sources/GlacioCoreWrapper", publicHeadersPath: "" ),
         .binaryTarget(name: "GlacioCore",
-                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.1.1/GlacioCore-pre-alpha-v0.1.1.zip",
+                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.11.0/GlacioCore-pre-alpha-v0.11.0.zip",
                       checksum: "316e2d04d5bbb93fd9e618c290d26f3f51fc228a458c0bc2b979f15b13e7b09e"),
         .binaryTarget(name: "MessagePack",
-                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.1.1/MessagePack-pre-alpha-v0.1.1.zip",
+                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.11.0/MessagePack-pre-alpha-v0.11.0.zip",
                       checksum: "85bc30abe460879ec679a762d691d4eef0331d562a1d2c58f373a3dc2f0c48c9"),
         .binaryTarget(name: "ArgumentParser",
-                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.1.1/ArgumentParser-pre-alpha-v0.1.1.zip",
+                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.11.0/ArgumentParser-pre-alpha-v0.11.0.zip",
                       checksum: "7aa9436715f39019c167704191c7393d815b3c08640658b7e012b391bd4347b0"),
         .binaryTarget(name: "ArgumentParserToolInfo",
-                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.1.1/ArgumentParserToolInfo-pre-alpha-v0.1.1.zip",
+                      url: "https://github.com/glaciotech/GlacioCore/releases/download/pre-alpha-v0.11.0/ArgumentParserToolInfo-pre-alpha-v0.11.0.zip",
                       checksum: "b5ecd6e68a142d245ca82da86a5decf048f4aab07e8356d12a37452009c72f39"),
     ]
 )
-
